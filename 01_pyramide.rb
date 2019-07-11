23baa318c040164764c6c0b94d1228d0cad843dc
chris <chrisleon325@gmail.com>
2019-07-11 13:47:13 +0300

    ajout d'un pyramide

---
+++ 01_pyramide.rb
@@ -0,0 +1,50 @@
+def half_pyramid
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+puts ">"
+nbr = gets.chomp.to_i
+l = " "
+base = "*"
+puts "Voici la pyramide :"
+  for etage in 1..nbr
+      puts "#{l*nbr + base}"
+      nbr = nbr - 1
+      base = base + "*"
+  end
+end
+
+def full_pyramid
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+puts ">"
+nbr = gets.chomp.to_i
+l = " "
+base = "*"
+puts "Voici la pyramide :"
+  for etage in 1..nbr
+      puts "#{l*nbr + base}"
+      nbr = nbr - 1
+      base = base + "**"
+  end
+end
+
+def wtf_pyramid 
+  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+  puts ">"
+  nbr = gets.chomp.to_i
+  l = " "
+  base = "*"
+  while nbr.even?
+    if nbr.even?
+      puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+      nbr = gets.chomp.to_i
+    elsif nbr.even
+      puts "Voici la pyramide :impaire"
+      for etage in 1..nbr
+        puts "#{l*nbr + base}"
+        nbr = nbr - 1
+        base = base + "**"
+      end
+    end
+  end
+end
+wtf_pyramid 
+
