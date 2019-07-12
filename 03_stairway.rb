c0498230c52ee89d6503a8c11ada6136a9185ad6
chris <chrisleon325@gmail.com>
2019-07-12 11:50:03 +0300

    mise à jour

--- 03_stairway.rb
+++ 03_stairway.rb
@@ -1,54 +1,47 @@
-def affich_staire(n,pos)
-a=Array(n)
-k="#"
-a[1]="#{k}"
-o="o"
-s="  "
-for i in 1..n
-   if (n-pos+1==i)       
-    puts"     #{s*(n-i)}#{o}#{a[i]}"   
-  else
-      puts"      #{s*(n-i)}%s" % [a[i]]
-   end
-   a[i+1]="#{k}#{a[i]}"
+def rool_dice
+	return rand (6)+1
 end
+rool_dice 
+
+def analyze_dice (dice)
+	if dice >= 5
+		puts "Vous avancez!"
+	    return 1
+    elsif dice == 1
+    puts "Vous reculez!"
+    	return-1
+    else 
+    	puts "Rien ne se passe"
+    	return 0
+    end
 end
-#affich_staire(6,4)
-def jouer()
-puts "Appuyez sur j pour jouer ,pour quitter cliquer autre"
-   puts ">"
-   a=gets.chomp
-   nb=1
-   pos=1
-   while (a=="j")
-       puts "vous avez jouer #{nb} fois"
-         if (pos==10)
-             puts "BRAVO   BRAVO   BRAVO   BRAVO"
-             puts "BRAVO   BRAVO   BRAVO   BRAVO"
-             puts "BRAVO   BRAVO   BRAVO   BRAVO"
-             puts "BRAVO   BRAVO   BRAVO   BRAVO"
-             puts "vous avez gagner"
-             break;
-         else
-             b=[1,2,3,4,5,6]
-              x=b.shuffle
-              if (x[1]==5 or x[1]==6)
-                  pos=pos+1
-                  puts"bravo tu avances d'une pas !"
-              elsif (x[1]==1)
-                  if pos !=1
-                      puts"oooooooh! dommage!!"
-                      pos=pos-1
-                  end
-                else
-                  puts"pfft"
-                end
-              affich_staire(10,pos)
-           puts "Appuyez sur j pour jouer ,pour quitter cliquer autre"
-           print ">"
-           a=gets.chomp
-         end
-      nb +=1
-   end
- end
-jouer
+
+def show_state (num)
+	puts "Vous êtes sur la marche n° #{num}"
+end
+
+def is_over? (num)
+	if num == 10
+		return true
+	else 
+		return false
+	end
+	
+end
+
+def play
+	puts "Bienvenue dans le jeu!"
+
+	step = 1
+
+	show_state(step)
+	while (!is_over?(step)) do
+	puts "taper 'entrée pour jouer"
+	gets.chomp
+	step = step + analyze_dice(rool_dice)
+	show_state (step)
+	end
+
+	puts"===== Vous avez gagné ====="
+end
+play
