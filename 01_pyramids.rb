cdccf4b179a3f859ded5c11b5153f1130153f9ac
chris <chrisleon325@gmail.com>
2019-07-12 10:46:24 +0300

    maj

--- 01_pyramids.rb
+++ 01_pyramids.rb
@@ -1,7 +1,10 @@
-def half_pyramid
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-puts ">"
-nbr = gets.chomp.to_i
+def entrer
+  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+  print ">"
+  return gets.chomp.to_i
+end
+
+def half_pyramid 
 l = " "
 base = "*"
 puts "Voici la pyramide :"
@@ -12,39 +15,55 @@ puts "Voici la pyramide :"
   end
 end
 
-def full_pyramid
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-puts ">"
-nbr = gets.chomp.to_i
-l = " "
-base = "*"
-puts "Voici la pyramide :"
-  for etage in 1..nbr
-      puts "#{l*nbr + base}"
-      nbr = nbr - 1
-      base = base + "**"
+def build_floor(nbr,base)
+  print " "*(base-nbr)
+  print "#"*(2*nbr-1)
+  puts
+end
+# faire  un boucle qui construit le pyramide
+def build_pyramid(nbr)
+  puts "Voici la pyramide :"
+  nbr.times do |base|
+    build_floor(base+1,nbr)
   end
 end
 
+def full_pyramid
+  build_pyramid(entrer)
+end
+
 def wtf_pyramid 
-  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-  puts ">"
-  nbr = gets.chomp.to_i
-  l = " "
-  base = "*"
-  while nbr.even?
-    if nbr.even?
-      puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-      nbr = gets.chomp.to_i
-    else
-      puts "Voici la pyramide :impaire"
-      for etage in 1..nbr
-        puts "#{l*nbr + base}"
-        nbr = nbr - 1
-        base = base + "**"
-      end
-    end
+  full_pyramid
+
+end
+wtf_pyramid
+=begin def wtf_pyramid
+    def ambany(etage)
+    l = (l/2)-1
+    pierre = "#"
+   l = " "
+    i = l
+   while i >= 0
+       
+     puts espace*(etage-i) + " "+" " + pierre*i + pierre*i + "#"
+     i = i - 1
+   end
+   def build_floor(total_floor,current_floor)
+  print " "*(current_floor-total_floor)
+  print "#"*(2*total_floor-1)
+  puts
+end
+# faire  un boucle qui construit le pyramide
+def build_pyramid(total_floor)
+  puts "Voici la pyramide :"
+
+  total_floor.times do |current_floor|
+    build_floor(current_floor+1,total_floor)
   end
 end
-wtf_pyramid 
+
+def full_pyramid
+  build_pyramid(ask_floor)
+end
+=end
 
